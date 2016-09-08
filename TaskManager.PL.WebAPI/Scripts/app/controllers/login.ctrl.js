09df84a9765def60e6d405062c2ba89e776cfa5d
kolomietsvv <kolomiets.victoriya@gmail.com>
2016-09-08 21:44:45 +0300

    fixed bug with autoLogout after page refresh

--- TaskManager.PL.WebAPI/Scripts/app/controllers/login.ctrl.js
+++ TaskManager.PL.WebAPI/Scripts/app/controllers/login.ctrl.js
@@ -12,6 +12,7 @@ App.controller('LoginCtrl',
             .then(function (res) {
                 var data = res.data;
                 angular.copy(data, userData);//Складываем данные из объекта дата в объект юзерДата(не ломая ссылку на объект юзерДата) п.с. на стадии ран
+                localStorage.setItem('userData', JSON.stringify(userData));
 
                 userLoaded.deferred().resolve();
                 $state.go("userpage");
--- TaskManager.PL.WebAPI/Scripts/app/run/checkAuth.js
+++ TaskManager.PL.WebAPI/Scripts/app/run/checkAuth.js
@@ -1,11 +1,17 @@
 ﻿'use strict';
 App.run(['$http', 'userData', 'userLoaded', function ($http, userData, userLoaded) {
+    //to do: get userData from local storage
+    var savedUserData = localStorage.getItem('userData');   
+    if (savedUserData)
+        angular.copy(JSON.parse(savedUserData), userData);
+
     $http.post('Account/IsAuthorized')
     .then(function (res) {
         var data = res.data;
         angular.copy(data, userData);//Складываем данные из объекта дата в объект юзерДата(не ломая ссылку на объект юзерДата) п.с. на стадии ран
         console.dir(userData);
         userLoaded.deferred().resolve();
+
     }, function () {
 
     });
